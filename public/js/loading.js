51a97c00a278fccc5582c5b68d50ac750d3ae21c
Joan <Joanpadolina@gmail.com>
2020-03-24 11:17:23 +0100

    loading...

---
+++ public/js/loading.js
@@ -0,0 +1,13 @@
+console.log('loading')
+document.onreadystatechange = function () {
+    var state = document.readyState
+    if (state == 'interactive') {
+         document.getElementById('content').style.visibility="hidden";
+    } else if (state == 'complete') {
+        setTimeout(function(){
+           document.getElementById('interactive');
+           document.getElementById('load').style.visibility="hidden";
+           document.getElementById('content').style.visibility="visible";
+        },200);
+    }
+  }
--- rollup.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-    input: 'src/css/*.css',
-    output: {
-      file: 'public/css/bundle-css.css',
-      format: 'iife'
-    }
-}
